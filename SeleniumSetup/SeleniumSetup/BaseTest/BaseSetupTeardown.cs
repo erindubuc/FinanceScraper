bfe916515ec36974de17d458cc1119e42d88284f
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-03-21 16:17:20 -0400

    Selenium added login, submitted, moved to password page

---
+++ SeleniumSetup/SeleniumSetup/BaseTest/BaseSetupTeardown.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumSetup.BaseTest
+{
+    public class BaseSetupTeardown
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Open()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://login.yahoo.com/";
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
