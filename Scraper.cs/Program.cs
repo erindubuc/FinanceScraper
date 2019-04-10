d46d4999b3a8e6e75a44af6201c6f6d284bc19de
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-04-10 11:40:52 -0400

    Folder setup

---
+++ Scraper.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using System.Data.SqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Scraper
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WebDriver driver = new WebDriver();
+            User user = new User();
+
+            WebDriver.DriverLoginToPortfolioAndGetStockData();
+
+            Console.ReadLine();
+
+        }
+    }
+}
